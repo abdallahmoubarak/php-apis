11c8067ad5a055b5e05aaf2e167d5da83fcff720
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-13 09:54:18 +0300

    christmass

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,18 @@ window.addEventListener("DOMContentLoaded", () => {
 
   palindromeBtn.addEventListener("click", () => {
     var input = document.getElementById("palindrome-input");
-    fetch(`localhost/api?string=${input}`)
+    fetch(`localhost/api/palindrome?string=${input}`)
       .then((response) => response.json())
       .then((data) => (isPalindrome.innerHTML = data.isPalindrome));
+  });
+
+  // palindrome
+  const christBtn = document.getElementById("christ-btn");
+  const days = document.getElementById("days");
+
+  christBtn.addEventListener("click", () => {
+    fetch(`localhost/api/christmas`)
+      .then((response) => response.json())
+      .then((data) => (days.innerHTML = data.days));
   });
 });
